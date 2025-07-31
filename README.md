bc2b827a56bcd90c4442aaba69cc541ccc1073a5
tkdeshan <tkdeshan1103@gmail.com>
2025-07-31 12:59:37 +0530

    modify map integration

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # ER-Website
+
+KMZ file viewer - https://earth.google.com/web/
---
+++ asset/landscape/landscape.js
@@ -0,0 +1,38 @@
+export const geojsonFiles = {
+    '1': {
+        name: 'Project 1 | Mirahawatte Belipola',
+        boundaries: 'asset/landscape/project1/ER94UP MirahawatteBelipola1.json',
+        plants: 'asset/landscape/project1/ER94UP MirahawatteBelipola2.json',
+        center: [80.94833, 6.86111],
+        zoom: 15,
+        maxZoom: 21,
+        bounds: [
+            [80.93806, 6.85444],
+            [80.96028, 6.86583]
+        ]
+    },
+    '2': {
+        name: 'Project 2',
+        boundaries: null,
+        plants: null,
+        center: [80.94833, 6.86111],
+        zoom: 15,
+        maxZoom: 21,
+        bounds: [
+            [80.9414, 6.8548],
+            [80.955, 6.865]
+        ]
+    },
+    '3': {
+        name: 'Project 3 | Beragala',
+        boundaries: null,
+        plants: 'asset/landscape/project3/Project 3.json',
+        center: [80.9386, 6.755],
+        zoom: 10,
+        maxZoom: 21,
+        bounds: [
+            [80.93444, 6.75278],
+            [80.94250, 6.75694]
+        ]
+    }
+};
